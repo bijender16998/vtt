7671d04c7a44765549effec76e738f105a47f7a4
Bijender Singh <bsbijender01@gmail.com>
2022-02-03 16:11:29 +0530

    text-to-speech updated for english and hindi

--- text-to-speech/text_to_speech.py
+++ text-to-speech/text_to_speech.py
@@ -1,17 +1,15 @@
-import pyttsx3
-engine = pyttsx3.init()
-try:
-    while True:
-        text=input("enter some text..\n")
-        try:
-            engine.say(text)
-            engine.setProperty('rate', 100)
-            engine.runAndWait()
-        except Exception as e:
-            print(e)
-            break
-except KeyboardInterrupt:
-    pass
+import os
+from gtts import gTTS
+import codecs
+from dotenv import load_dotenv
+load_dotenv()
+RATE = os.getenv('RATE')
+language=os.getenv('language')
+text_file=os.getenv('text_file')
 
-
-
+with codecs.open(text_file, encoding='utf-8') as f:
+     input=f.read()
+     speech = gTTS(text=input, lang=language, slow=False)
+     audio_file=f"{text_file.split('.')[0]}"+'.wav'
+     speech.save(audio_file)
+     os.system(f'start {audio_file}')
